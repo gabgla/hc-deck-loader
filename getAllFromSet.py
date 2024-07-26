6de8ecb22d0543d128376d5a58064e2564a97019
gabgla <gabggl20@gmail.com>
2024-07-26 23:22:07 +0100

    Get cards from set tool

---
+++ getAllFromSet.py
@@ -0,0 +1,17 @@
+import requests
+import re
+import json
+
+PATTERN = r"JSON\.parse\('(.*)'\)}}]\);"
+URL = "https://raw.githubusercontent.com/bones-bones/hellfall/main/src/data/Hellscube-Database.json"
+SET = "HC4"
+
+response = requests.get(URL)
+response.content
+
+parsed = json.loads(response.content)
+
+for card in parsed["data"]:
+    if card["Set"] != SET:
+        continue
+    print("1 " + card["Name"])
