6b19520e74d1afb3b152040a512d81fdbcf42de0
gabgla <gabggl20@gmail.com>
2024-08-17 01:23:44 +0100

    Build script cleanup

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 VERSION=$(jq -r .version meta.json)
+SCRIPT_PATH=./dist/main.lua
 DIST_OBJ_PATH=./dist/HC_Deck_Loader_v$VERSION.json
 
 mkdir dist 2> /dev/null
-python3 generate_code.py > ./dist/main.lua
-
-NEW_SCRIPT=$(<./dist/main.lua)
+python3 generate_code.py > $SCRIPT_PATH
 
 cp ./src/HC_Deck_Loader_Template.json ${DIST_OBJ_PATH}.tmp
-# sed -i "s/<<LUASCRIPT>>/$(jq -R -s '.' < ./dist/main.lua)/g" $DIST_OBJ_PATH
-jq '.ObjectStates[0].LuaScript = $newscript' --rawfile newscript ./dist/main.lua $DIST_OBJ_PATH.tmp > $DIST_OBJ_PATH
+jq '.ObjectStates[0].LuaScript = $newscript' --rawfile newscript $SCRIPT_PATH $DIST_OBJ_PATH.tmp > $DIST_OBJ_PATH
 rm $DIST_OBJ_PATH.tmp
